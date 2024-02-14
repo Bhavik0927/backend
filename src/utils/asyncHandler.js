1ba04a07fd4d43031bab2f128a445b9a526204b7
Bhavik <bhaviktembhare2@gmail.com>
2024-02-14 11:33:48 +0530

    utils file created

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,26 @@
+// for better underStanding get an help of chatGPT
+const asyncHandler = (requestHandler) =>{
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+
+/* const asyncHandler = () => {}
+const asyncHandler = (fn) => () =>{}
+const asyncHandler = (fn) => { () => {} }  //Higher order function */
+
+//Try catch method 
+/* const asyncHandler = (fn) => async (req,res,next) => {
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+        res.status(err.code || 500).json({
+            success:false,
+            message:err.message
+        })
+    }
+} */
